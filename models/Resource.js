5cec1fecd9907f752079e9ddc87afa918de76c28
Navneet Singh <200413180@student.georgianc.on.ca>
2020-06-23 16:18:51 -0400

    hi

---
+++ models/Resource.js
@@ -0,0 +1,54 @@
+// INSTRUCTIONS
+/*
+  Create a new resource model that uses the User
+  as an associative collection (examples):
+  - User -> Books
+  - User -> Reservation
+
+  Your model must contain at least three attributes
+  other than the associated user and the timestamps.
+
+  Your model must have at least one helpful virtual
+  or query function. For example, you could have a
+  book's details output in an easy format: book.format()
+*/
+
+const mongoose = require('mongoose');
+
+const ResourceSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true // This must exist
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: String,
+    required: false
+  }
+}, {
+  timestamps: true
+});
+
+// Query Helpers
+
+ResourceSchema.virtual('synopsis')
+.get(function () {
+  const post = this.content;
+  return post
+    .replace(/(<([^>]+)>)/ig,"")
+    .substring(0, 250);
+});
+
+module.exports = mongoose.model('Resource', ResourceSchema);
